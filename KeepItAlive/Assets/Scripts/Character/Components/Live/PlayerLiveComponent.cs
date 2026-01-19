2a9ce5d18609150410b26ac47ef017cf4c5246e0
Aliya <azunusov4@gmail.com>
2026-01-19 22:59:10 +0800

    Update PlayerLiveComponent.cs

--- KeepItAlive/Assets/Scripts/Character/Components/Live/PlayerLiveComponent.cs
+++ KeepItAlive/Assets/Scripts/Character/Components/Live/PlayerLiveComponent.cs
@@ -20,7 +20,7 @@ public class PlayerLiveComponent : ILiveComponent
         private set
         {
             health = value;
-            if (health < 0)
+            if (health <= 0)
             {
                 health = 0;
                 SetDeath();
